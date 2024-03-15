2d76f21ae78c2ebc9611a18b62be60f6dfb2a903
Maithri <m.muralikrishna.cse21@itbhu.ac.in>
2024-03-15 08:46:12 +0530

    Added pages for login and register

---
+++ vtpfrontend/src/views/HomePage/homepage.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing if necessary
+import './homepage.css'
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    // Logic for handling login
+    console.log('Logging in with:', email, password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+}
---
+++ vtpfrontend/src/views/Register/register.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing if necessary
+import './register.css'
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    // Logic for handling registration
+    console.log('Registering with:', name, email, password);
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+      <div>
+        <p>Already have an account? <Link to="/
+        ">Login</Link></p>
+      </div>
+    </div>
+  );
+}
