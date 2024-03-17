f9108eae54b531747f08572b612c3d9cb9a56e24
Tanishka Gupta <130149454+tanishkagupta2207@users.noreply.github.com>
2024-03-17 21:04:20 +0530

    Add files via upload

---
+++ vtpfrontend/src/api/togglePortfolio.js
@@ -0,0 +1,14 @@
+import axiosInstance from './axiosInstance';
+import { AxiosError } from 'axios';
+
+export const togglePortfolio = async () => {
+  try {
+    const res = await axiosInstance.get(`/portfolio/status`);
+    return res.data;
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      return e.response?.data || e.message;
+    }
+    throw e;
+  }
+};
