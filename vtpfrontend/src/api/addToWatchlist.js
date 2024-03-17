5b0e2c7fbcab7fdddf6b4153749cf1fbd4113dd6
Tanishka Gupta <130149454+tanishkagupta2207@users.noreply.github.com>
2024-03-17 20:50:33 +0530

    Add files via upload

---
+++ vtpfrontend/src/api/addToWatchlist.js
@@ -0,0 +1,14 @@
+import axiosInstance from './axiosInstance';
+import { AxiosError } from 'axios';
+
+export const addToWatchlist = async (symbol) => {
+  try {
+    const res = await axiosInstance.post(`/watchlist/add`, { symbol });
+    return res.data;
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      return e.response?.data || e.message;
+    }
+    throw e;
+  }
+};
