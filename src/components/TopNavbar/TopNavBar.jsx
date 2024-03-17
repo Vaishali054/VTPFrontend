2fd067a9b7c9ad273fb668bfd392aea1b863945c
Tanishka Gupta <130149454+tanishkagupta2207@users.noreply.github.com>
2024-03-17 21:25:14 +0530

    Update TopNavBar.jsx

--- src/components/TopNavbar/TopNavBar.jsx
+++ src/components/TopNavbar/TopNavBar.jsx
@@ -6,59 +6,119 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-
+import { fetchProfile } from '../../api/fetchProfile';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import { useNavigate } from 'react-router-dom';
 
 export default function TopNavBar() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const navigate=useNavigate();
+  const [anchorElMenu, setAnchorElMenu] = React.useState(null);
+  const [anchorElAccount, setAnchorElAccount] = React.useState(null);
+  const navigate = useNavigate();
+  const [userId, setuserId] = React.useState('');
 
   const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+    setAnchorElMenu(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseMenu = () => {
+    setAnchorElMenu(null);
   };
 
-  const handleProfile=()=>{
-    setAnchorEl(null);
+  const handleAccount = (event) => {
+    setAnchorElAccount(event.currentTarget);
+  };
+
+  const handleCloseAccount = () => {
+    setAnchorElAccount(null);
+  };
+
+  const handleProfile = () => {
+    setAnchorElAccount(null);
     navigate("/profile");
-  }
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const data = await fetchProfile();
+      if (data) {
+        setuserId(data.user.id);
+      } else {
+        console.error('Failed to fetch user data');
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  React.useEffect(() => {
+    // Fetch user data when the component mounts
+    fetchUserData();
+  }, []);
+
+  const handleWatchlist = () => {
+    setAnchorElMenu(null);
+    navigate(`/watchlist`);
+  };
+
+  const handlePortfolio = () => {
+    setAnchorElMenu(null);
+    if (userId) {
+      navigate(`/portfolio/${userId}`);
+    } else {
+      console.error('User ID is not available');
+    }
+  };
 
   return (
     <div>
-    <Box sx={{ flexGrow: 1 }}>
-     
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMenu}
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElMenu}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElMenu)}
+              onClose={handleCloseMenu}
+            >
+              <MenuItem onClick={handleWatchlist}>Watchlist</MenuItem>
+              <MenuItem onClick={handlePortfolio}>Portfolio</MenuItem>
+            </Menu>
+            <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
             </Typography>
             <div>
               <IconButton
                 size="large"
                 aria-label="account of current user"
-                aria-controls="menu-appbar"
+                aria-controls="menu-appbar-account"
                 aria-haspopup="true"
-                onClick={handleMenu}
+                onClick={handleAccount}
                 color="inherit"
               >
-            <AccountCircle />
+                <AccountCircle />
               </IconButton>
               <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
+                id="menu-appbar-account"
+                anchorEl={anchorElAccount}
                 anchorOrigin={{
                   vertical: 'top',
                   horizontal: 'right',
@@ -68,17 +128,17 @@ export default function TopNavBar() {
                   vertical: 'top',
                   horizontal: 'right',
                 }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
+                open={Boolean(anchorElAccount)}
+                onClose={handleCloseAccount}
               >
                 <MenuItem onClick={handleProfile}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleCloseAccount}>My account</MenuItem>
               </Menu>
             </div>
-        </Toolbar>
-      </AppBar>
-      <Toolbar /> 
-    </Box>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+      </Box>
     </div>
   );
 }
