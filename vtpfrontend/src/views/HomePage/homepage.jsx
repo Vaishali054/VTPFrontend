b70ac83ffaaac0ab7baf6db00b430def5619a72d
Maithri <m.muralikrishna.cse21@itbhu.ac.in>
2024-03-17 16:31:32 +0530

    Added history and modified login, register pages

--- vtpfrontend/src/views/HomePage/homepage.jsx
+++ vtpfrontend/src/views/HomePage/homepage.jsx
@@ -1,27 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
 import './homepage.css';
+import TopNavBar from '../../components/TopNavbar/TopNavBar';
+import { handleLogin } from '../../api/loginauth'; // Import the API function for login
 
 export default function Login() {
   const [email_id, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleLogin = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3080/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email_id, password }),
-      }); 
+      const response = await handleLogin(email_id, password); // Call the API function for login
       if (response.ok) {
-        console.log('Login successful');
+        // console.log('Login successful');
+        window.location.href = '/auth/profile';
       } else {
-        const data = await response.json();
-        setError(data.message);
+        setError(response.message);
       }
     } catch (error) {
       console.error('Error during login:', error);
@@ -30,33 +26,36 @@ export default function Login() {
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      {error && <div className="error-message">{error}</div>}
-      <form onSubmit={handleLogin}>
+    <>
+       <TopNavBar />
+      <div className="login-container">
+        <h2>Login</h2>
+        {error && <div className="error-message">{error}</div>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email_id"
+              value={email_id}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
         <div>
-          <label>Email ID:</label>
-          <input
-            type="email_id"
-            value={email_id}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <p>Don't have an account? <Link to="/register">Register</Link></p>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <div>
-        <p>Don't have an account? <Link to="/register">Register</Link></p>
       </div>
-    </div>
+    </>
   );
 }
--- vtpfrontend/src/views/Register/register.jsx
+++ vtpfrontend/src/views/Register/register.jsx
@@ -1,75 +1,112 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './register.css';
+import TopNavBar from '../../components/TopNavbar/TopNavBar';
+import { registerUser } from '../../api/register';
 
 export default function Register() {
   const [name, setName] = useState('');
   const [email_id, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleRegister = async (event) => {
     event.preventDefault();
     console.log('Registering with:', name, email_id, password);
 
-    try {
-      const response = await fetch('http://localhost:3080/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email_id, password }),
-      });
+    if (password !== confirmPassword) {
+      console.error('Password and confirm password do not match.');
+      return;
+    }
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Registration successful:', data);
-        window.location.href = '/'; 
+    try {
+      const { success, message, error } = await registerUser(name, email_id, password);
+      if (success) {
+        window.location.href = '/';
       } else {
-        console.error('Registration failed');
+        window.location.href = '/';
+        if (error && error.response && error.response.data && error.response.data.message === 'Email already in use') {
+          console.error('Email ID is already in use. Please use a different one.');
+        } else {
+          console.error(message || 'Registration failed. Please try again.');
+        }
       }
     } catch (error) {
       console.error('Error registering user:', error);
+      console.error('Internal server error. Please try again later.');
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
-    <div className="register-container">
-      <h2>Register</h2>
-      <form onSubmit={handleRegister}>
+    <>
+      <TopNavBar />
+      <div className="register-container">
+        <h2>Register</h2>
+        <form onSubmit={handleRegister}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Email ID:</label>
+            <input
+              type="email"
+              value={email_id}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="password-input">
+            <label>Password:</label>
+            <div className="password-input-container">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button type="button" onClick={togglePasswordVisibility}>
+                {showPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+          </div>
+          <div className="confirm-password">
+            <label>Confirm Password:</label>
+            <div className="password-input-container">
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+              <button type="button" onClick={toggleConfirmPasswordVisibility}>
+                {showConfirmPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+          </div>
+          <button type="submit">Register</button>
+        </form>
         <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
+          <p>
+            Already have an account? <Link to="/">Login</Link>
+          </p>
         </div>
-        <div>
-          <label>Email ID:</label>
-          <input
-            type="email"
-            value={email_id}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-      <div>
-        <p>
-          Already have an account? <Link to="/login">Login</Link>
-        </p>
       </div>
-    </div>
+    </>
   );
 }
